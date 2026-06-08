97565ff301ac78064bd8e8aaaa5ccbd7663ff095
Roman_Chub <rchub@inbox.ru>
2026-06-08 09:45:57 +0300

    display_users task have been completed

---
+++ function-map/display_users.js
@@ -0,0 +1,22 @@
+const getChildren = (users) => users.flatMap((user) => user.children);
+
+const users = [
+  {
+    name: "Tirion",
+    children: [{ name: "Mira", birthday: "1983-03-23" }],
+  },
+  { name: "Bronn", children: [] },
+  {
+    name: "Sam",
+    children: [
+      { name: "Aria", birthday: "2012-11-03" },
+      { name: "Keit", birthday: "1933-05-14" },
+    ],
+  },
+  {
+    name: "Rob",
+    children: [{ name: "Tisha", birthday: "2012-11-03" }],
+  },
+];
+
+console.log(getChildren(users));
